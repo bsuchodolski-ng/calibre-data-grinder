de2fe33ca819e0fbf28b28f4da7fe1ef06365b1a
bsuchodolski-ng <bartosz.suchodolski@netguru.pl>
2018-05-02 22:13:23 +0200

    Configure redis as cache

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,8 @@ module CalibreDataGrinder
         end
       end
     end
+
+    # Use redis as cache store
+    config.cache_store = :redis_store, "redis://localhost:6379/0/cache", { expires_in: 90.minutes }
   end
 end
