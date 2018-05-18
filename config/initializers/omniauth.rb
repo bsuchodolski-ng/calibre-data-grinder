d20723aaddd4aa29f7d3eb0e58afe6116617c835
bsuchodolski-ng <bartosz.suchodolski@netguru.pl>
2018-05-18 09:10:36 +0200

    Provide google domain as env variable

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['GOOGLE_CLIENT_API'], ENV['GOOGLE_CLIENT_SECRET'], hd: 'netguru.pl', prompt: 'select_account'
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_API'], ENV['GOOGLE_CLIENT_SECRET'], hd: ENV['GOOGLE_DOMAIN'], prompt: 'select_account'
 end
